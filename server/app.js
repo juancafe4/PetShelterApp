4dc62fd79aa94c1b8447a8293669e13d79883bba
Juan Carlos <juancafe2@gmail.com>
2016-08-25 10:35:35 -0700

    Fixing heroku

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 // CONSTANTS
 const PORT = process.env.PORT || 8000;
-const MONGO_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/SHELTER_DB';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/SHELTER_DB';
 
 // PACKAGE REQUIRES
 const bodyParser = require('body-parser');
